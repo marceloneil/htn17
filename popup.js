3713e502c63313d1e05d2be247d170478deea2f0
Wuon <wuonlabs@gmail.com>
2017-09-16 15:57:12 -0400

    cam fix

--- popup.js
+++ popup.js
@@ -137,31 +137,15 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     toggle.addEventListener('click', () => {
         if(state){
-            startCamera();
             state = 0;
+            startCamera();
         }else{
-            endCamera();
             state=1;
+            endCamera();
         }
     });
 
   });
-});
-
-$('.button').mousedown(function (e) {
-    var target = e.target;
-    var rect = target.getBoundingClientRect();
-    var ripple = target.querySelector('.ripple');
-    $(ripple).remove();
-    ripple = document.createElement('span');
-    ripple.className = 'ripple';
-    ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
-    target.appendChild(ripple);
-    var top = e.pageY - rect.top - ripple.offsetHeight / 2 -  document.body.scrollTop;
-    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
-    ripple.style.top = top + 'px';
-    ripple.style.left = left + 'px';
-    return false;
 });
 
 window.onload = function() {
