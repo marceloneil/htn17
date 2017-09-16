495450ab238982b43e9d88a475558164277d09f1
Marcel O'Neil <marcel@marceloneil.com>
2017-09-16 17:28:52 -0400

    Requests

--- event.js
+++ event.js
@@ -1,27 +1,18 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.get === 'image') {
-    chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-      alert(screenshotUrl);
-    })
-  }
-});
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   if (request.get === 'image') {
+//     chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+//       alert(screenshotUrl);
+//     })
+//   }
+// });
 
 setTimeout(function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {get: 'data'}, function(response) {
-      alert(JSON.stringify(request));
-      request
-        .post('https://htn17-processing-kshen3778.c9users.io/pushToFirebase')
-        .send(response)
-        .end(function(err, res){
-          alert(err);
-          alert(res);
-          if (err || !res.ok) {
-            alert('Oh no! error');
-          } else {
-            alert('yay got ' + JSON.stringify(res.body));
-          }
-        });
-      });
+      console.log(response);
+      $.post('https://htn17-processing-kshen3778.c9users.io/createHeatMap', response, function(data) {
+        console.log(data)
+      }, 'json');
+    })
   });
-}, 30000);
+}, 10000);
