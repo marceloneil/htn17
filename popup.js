6ec1c8b76d78c9e78788eb4dd857b029ed4d5c17
Wuon <wuonlabs@gmail.com>
2017-09-16 23:42:13 -0400

    ui upgrade

--- popup.js
+++ popup.js
@@ -123,20 +123,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Load the saved background color for this page and modify the dropdown
     // value, if needed.
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-
-      }
-    });
 
     // Ensure the background color is changed and saved when the dropdown
     // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
     options.addEventListener('click', () => {
         console.log("xfer");
         if (chrome.runtime.openOptionsPage) {
