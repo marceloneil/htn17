f4dc678248c6ea1b9e22ccac17dce3b89cf2c3c4
Marcel O'Neil <marcel@marceloneil.com>
2017-09-17 08:03:16 -0400

    Screenshots & requests

--- popup.js
+++ popup.js
@@ -1,320 +1,72 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
- /**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.query({
     active: true,
     currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.tabs.captureVisibleTab(function(screenshotUrl) {
-    //alert(screenshotUrl);
-  })
-  getCurrentTabUrl((url) => {
+  }, function (tabs) {
     var dropdown = document.getElementById('dropdown');
     var options = document.getElementById('options');
     var toggle = document.getElementById('toggle');
     var state = false;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {get: "state"}, function(response) {
-            state = response;
-            document.getElementById("toggle").checked = state;
-            console.log(state);
+
+    chrome.tabs.sendMessage(tabs[0].id, {
+      get: "state"
+    }, function (response) {
+      if (response) {
+        state = response;
+      } else {
+        state = false;
+      }
+      document.getElementById("toggle").checked = state;
+      console.log('On:', state);
+    });
+
+    options.addEventListener('click', function () {
+      if (chrome.runtime.openOptionsPage) {
+        // New way to open options pages, if supported (Chrome 42+).
+        chrome.runtime.openOptionsPage();
+      } else {
+        // Reasonable fallback.
+        window.open(chrome.runtime.getURL('options.html'));
+      }
+    });
+
+    toggle.addEventListener('click', function () {
+      if (!state) {
+        state = true;
+        chrome.tabs.sendMessage(tabs[0].id, {
+          get: "start"
+        }, function (response) {
+          if (response) {
+            console.log(response.message);
+          }
         });
-    });
-
-
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    options.addEventListener('click', () => {
-        console.log("xfer");
-        if (chrome.runtime.openOptionsPage) {
-            // New way to open options pages, if supported (Chrome 42+).
-            chrome.runtime.openOptionsPage();
-        } else {
-            // Reasonable fallback.
-            window.open(chrome.runtime.getURL('options.html'));
-        }
-    });
-    toggle.addEventListener('click', () => {
-        console.log("click");
-        if(!state){
-            state = true;
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {get: "start"}, function(response) {
-                    console.log(response.message)
-                });
-
-            });
-        }else{
-            state = false;
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {get: "stop"}, function(response) {
-                    console.log(response.message);
-                    if(response.message){
-                        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                            chrome.tabs.sendMessage(tabs[0].id, {get: 'data'}, function(data) {
-                              takeScreenshot(data);
-                            })
-                        });
-                    }
-                });
-            });
-        }
+      } else {
+        state = false;
+        chrome.tabs.sendMessage(tabs[0].id, {
+          get: "stop"
+        }, function (data) {
+          console.log('stopping camera...');
+          CaptureAPI.captureToBase64(tabs[0], function (error, images) {
+            if (error) {
+              return console.error(error)
+            }
+            data.imgData = images[0];
+            sendData(data);
+          });
+        });
+      }
     });
 
   });
 });
 
-
-// Copyright (c) 2012,2013 Peter Coles - http://mrcoles.com/ - All rights reserved.
-// Use of this source code is governed by the MIT License found in LICENSE
-
-
-//
-// State fields
-//
-
-var currentTab, // result of chrome.tabs.query of current active tab
-    resultWindowId; // window id for putting resulting images
-
-
-//
-// Utility methods
-//
-
-function $(id) { return document.getElementById(id); }
-function show(id) {/* $(id).style.display = 'block'; */}
-function hide(id) {/* $(id).style.display = 'none'; */}
-
-
-function getFilename(contentURL) {
-    var name = contentURL.split('?')[0].split('#')[0];
-    if (name) {
-        name = name
-            .replace(/^https?:\/\//, '')
-            .replace(/[^A-z0-9]+/g, '-')
-            .replace(/-+/g, '-')
-            .replace(/^[_\-]+/, '')
-            .replace(/[_\-]+$/, '');
-        name = '-' + name;
-    } else {
-        name = '';
-    }
-    return 'screencapture' + name + '-' + Date.now() + '.png';
-}
-
-
-//
-// Capture Handlers
-//
-
-
-function displayCaptures(filenames) {
-    if (!filenames || !filenames.length) {
-        show('uh-oh');
-        return;
-    }
-
-    _displayCapture(filenames);
-}
-
-
-function _displayCapture(filenames, index) {
-    index = index || 0;
-
-    var filename = filenames[index];
-    var last = index === filenames.length - 1;
-
-    if (currentTab.incognito && index === 0) {
-        // cannot access file system in incognito, so open in non-incognito
-        // window and add any additional tabs to that window.
-        //
-        // we have to be careful with focused too, because that will close
-        // the popup.
-        chrome.windows.create({
-            url: filename,
-            incognito: false,
-            focused: last
-        }, function(win) {
-            resultWindowId = win.id;
-        });
-    } else {
-        /*chrome.tabs.create({
-            url: filename,
-            active: last,
-            windowId: resultWindowId,
-            openerTabId: currentTab.id,
-            index: (currentTab.incognito ? 0 : currentTab.index) + 1 + index
-        });*/
-    }
-
-    if (!last) {
-        _displayCapture(filenames, index + 1);
-    }
-}
-
-
-function errorHandler(reason) {
-    show('uh-oh'); // TODO - extra uh-oh info?
-}
-
-
-function progress(complete) {
-    if (complete === 0) {
-        // Page capture has just been initiated.
-        //show('loading');
-    }
-    else {
-        //$('bar').style.width = parseInt(complete * 100, 10) + '%';
-    }
-}
-
-
-function splitnotifier() {
-    show('split-image');
-}
-
-
-//
-// start doing stuff immediately! - including error cases
-//
-var captureF = true;
-
-var resetCapture = function(){
-  captureF = false;
-  setTimeout(function(){
-    captureF = true;
-  },10000);
-}
-var takeScreenshot = function(theData){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if(captureF){
-      //captureF = false;
-      resetCapture();
-      var tab = tabs[0];
-      currentTab = tab; // used in later calls to get tab info
-
-      var filename = getFilename(tab.url);
-      CaptureAPI.captureToFiles(tab, filename, displayCaptures,
-                                errorHandler, progress, splitnotifier, theData);
-    }
+function sendData(data) {
+  console.log('sending data...');
+  $.ajax({
+    type: 'POST',
+    url: 'https://htn17-processing-kshen3778.c9users.io/createHeatMap',
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    dataType: 'json'
   });
 }
-
-
-// $('.button').mousedown(function (e) {
-//     var target = e.target;
-//     var rect = target.getBoundingClientRect();
-//     var ripple = target.querySelector('.ripple');
-//     $(ripple).remove();
-//     ripple = document.createElement('span');
-//     ripple.className = 'ripple';
-//     ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
-//     target.appendChild(ripple);
-//     var top = e.pageY - rect.top - ripple.offsetHeight / 2 -  document.body.scrollTop;
-//     var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
-//     ripple.style.top = top + 'px';
-//     ripple.style.left = left + 'px';
-//     return false;
-// });
